9d80c5d3bf76b2f657a2d76ea8f630aaad4bd855
Antoine Chwat <chwat@MacBook-Air-de-Antoine.local>
2015-10-20 15:05:12 +0200

    hjh

--- AGP/TP/TP3/hanoi.c
+++ AGP/TP/TP3/hanoi.c
@@ -2,16 +2,16 @@
 #include "hanoi.h"
 
 void deplacer(PILE *source, PILE *dest) {
-	int piece=depiler(*source);
-	empiler(*dest);
+	int piece=depiler(source);
+	empiler(dest);
 }
 
 void hanoi(PILE *source, PILE *dest, PILE *passage, int N){
 	if (N>1) {
-		hanoi(*source, *passage, *dest, N-1);
-		deplacer(*source, *dest);
-		hanoi(*passage, *dest, *source, N-1);
+		hanoi(source, passage, dest, N-1);
+		deplacer(source, dest);
+		hanoi(passage, dest, source, N-1);
 	} else {
-		deplacer(*source, *dest);
+		deplacer(source, dest);
 	}
 }
