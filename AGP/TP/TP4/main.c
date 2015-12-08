d4b1a2cb0ef0936b3294ddd77eb0194a88f3ee9e
Antoine Chwat <chwat@MacBook-Air-de-Antoine.local>
2015-12-08 14:14:47 +0100

    Finished project

--- AGP/TP/TP4/main.c
+++ AGP/TP/TP4/main.c
@@ -4,21 +4,23 @@
 
 int main(){
 
-	NODE *forward=newNode("FORWARD", NULL, 1);
-	NODE *right=newNode("RIGHT", NULL, 1);
-	NODE *left=newNode("LEFT", NULL, 1);
-	NODE *repeat=newNode("REPEAT", newNode("REPEAT", newNode("FORWARD", NULL, 1), 1), 1);
+	NODE *forward=newNode("FORWARD", 1, NULL);
+	NODE *right=newNode("RIGHT", 1, NULL);
+	NODE *left=newNode("LEFT", 1, NULL);
+	NODE *repeat=newNode("REPEAT", 1, newNode("REPEAT", 1, newNode("FORWARD", 1, NULL)));
 	printf("\nNew nodes made\n");
 
 	NODE *program=newProgram(forward);
 	printf("New program made\n");
 
-	addNode(program, right, program);
-	addNode(program, left, right);
-	addNode(program, repeat, left);
+	addNode(program, right);
+	addNode(program, left);
+	addNode(program, repeat);
 	printf("Nodes added\n\n");
 
 	printProgram(program);
+
+	//draw(program);
 
 	memFree(program);
 }
